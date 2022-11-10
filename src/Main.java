f84f44a6520284b884dcf0686be4c3d5953443d5
lukaskolb <lordshattan@gmail.com>
2022-11-10 16:55:36 +0100

    P1: Add credentials valid method + main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        int maxAttempt = 3;
+        int currentAttempt = 0;
+        Scanner s = new Scanner(System.in);
 
+        while(currentAttempt < maxAttempt){
+
+            System.out.print("Benutzername: ");
+            String login = s.next();
+            System.out.print("Passwort: ");
+            String pwd = s.next();
+
+
+            Credentials userdata = new Credentials(login,pwd);
+
+            if(userdata.valid()){
+                s.close();
+                System.out.println("Success!");
+                currentAttempt = maxAttempt;
+            }
+            else{
+                currentAttempt ++;
+                if(currentAttempt == maxAttempt){
+                    System.out.println("Access denied!");
+                }
+            }
+        }
     }
 }
