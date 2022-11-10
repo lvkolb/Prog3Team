5bbace6fa3bc146104910394e95cdb1d101cc629
lukaskolb <lordshattan@gmail.com>
2022-11-10 16:34:19 +0100

    P1: Add credentials valid method

--- src/Credentials.java
+++ src/Credentials.java
@@ -3,16 +3,22 @@ public class Credentials {
     String hash;
 
     Credentials(String login, String hash){
+        Helpers helper = new Helpers();
         this.login = login;
-        this.hash = hash;
+        this.hash = helper.getHash(hash);
     }
 
-    public boolean valid (){
-        /*
-         *   String kombinieren
-         *   auf text Datei zugreifen und überprüfen ob der String enthalten ist
-         *
-         * */
-        return true;
+    public boolean valid(){
+        boolean checker = false;
+        Helpers helper = new Helpers();
+        String[] password = helper.readTextdatei("src/passwd.txt");
+        for(int i = 0; i < password.length; i++){
+            if(password[i].equals(login)){
+                if(password[i+1].equals(hash)){
+                    checker = true;
+                }
+            }
+        }
+        return checker;
     }
 }
